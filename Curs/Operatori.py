eecb4ec1eaa2094b010e73da3b1801057e23ce6d
TanaseRobert <roberttanasa01@yajoo.com>
2023-01-07 22:25:57 +0200

    Update Operatori.py

--- Curs/Operatori.py
+++ Curs/Operatori.py
@@ -11,6 +11,3 @@ print(a % b)                                 # 4
 print(a != b)                                # True
 print(a % b == 4 and a * b == 25)            # True and False -> False
 print(a / b == 0.8 or a > b)                 # True or False -> True
-
-
-#taaaaaa
